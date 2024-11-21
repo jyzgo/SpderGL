a1afa78568ddedd5f2303ce6a0e04edb31b7a1f4
ethan <jyzgo0125@gmail.com>
2024-11-22 01:07:23 +0800

    Build0.034bb

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Playtwist-SpiderNightmare-0.033b";
+const cacheName = "Playtwist-SpiderNightmare-0.034b";
 const contentToCache = [
     "Build/SpiderGL.loader.js",
     "Build/SpiderGL.framework.js",
