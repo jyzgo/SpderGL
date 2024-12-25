f574338c81727844ce04f0c8c037715930495fa1
� <>
2024-12-26 00:38:15 +0800

    Build0.141bb

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Playtwist-SpiderNightmare-0.14b";
+const cacheName = "Playtwist-SpiderNightmare-0.141b";
 const contentToCache = [
     "Build/SpiderGL.loader.js",
     "Build/SpiderGL.framework.js",
